eeccf31dfde1994f0948f6e09c64fa644c0e428d
CarlOsterberg <carl.vilhelms.osterberg@gmail.com>
2025-06-30 00:41:58 +0200

    initial

---
+++ public_include/matrix.h
@@ -0,0 +1,129 @@
+#ifndef MATRICES_LIBRARY_H
+#define MATRICES_LIBRARY_H
+
+#include <array>
+#include <cstdint>
+#include <algorithm>
+#include <iostream>
+#include <type_traits>
+
+template <typename T>
+concept Number = std::is_arithmetic_v<T>;
+template <Number T, uint32_t M, uint32_t N>
+class Matrix
+{
+    static_assert(std::is_arithmetic_v<T>);
+
+public:
+    Matrix() : m_matrix(std::array<std::array<T, M>, N>{})
+    {
+    }
+
+    Matrix(std::array<std::array<T, M>, N> a_matrix) : m_matrix(a_matrix)
+    {
+    }
+
+    void print()
+    {
+        std::stringstream ss;
+        ss << std::to_string(M) << " x " << std::to_string(N) << '\n';
+        for (uint32_t m = 0; m < M; m++)
+        {
+            for (uint32_t n = 0; n < N; n++)
+            {
+                if (n == 0)
+                {
+                    ss << '|';
+                }
+                else
+                {
+                    ss << '\t';
+                }
+                ss << std::to_string(m_matrix[n][m]);
+                if (n == N - 1)
+                {
+                    ss << '|';
+                }
+            }
+            ss << '\n';
+        }
+        std::cout << ss.str();
+    }
+
+    constexpr bool operator==(const Matrix& rhs) const
+    {
+        for (uint32_t m = 0; m < M; m++)
+        {
+            for (uint32_t n = 0; n < N; n++)
+            {
+                if (m_matrix[n][m] != rhs.m_matrix[n][m])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    constexpr bool operator!=(const Matrix& rhs) const
+    {
+        return not (*this == rhs);
+    }
+
+    constexpr Matrix operator+(const Matrix& rhs) const
+    {
+        Matrix matrix;
+        for (uint32_t m = 0; m < M; m++)
+        {
+            for (uint32_t n = 0; n < N; n++)
+            {
+                matrix.m_matrix[n][m] = m_matrix[n][m] + rhs.m_matrix[n][m];
+            }
+        }
+        return std::move(matrix);
+    }
+
+    constexpr Matrix operator-(const Matrix& rhs) const
+    {
+        Matrix matrix;
+        for (uint32_t m = 0; m < M; m++)
+        {
+            for (uint32_t n = 0; n < N; n++)
+            {
+                matrix.m_matrix[n][m] = m_matrix[n][m] - rhs.m_matrix[n][m];
+            }
+        }
+        return std::move(matrix);
+    }
+
+    constexpr Matrix<T, N, M> transpose() const
+    {
+        std::array<std::array<T, N>, M> array;
+        for (uint32_t m = 0; m < M; m++)
+        {
+            for (uint32_t n = 0; n < N; n++)
+            {
+                array[m][n] = m_matrix[n][m];
+            }
+        }
+        return {array};
+    }
+
+    constexpr Matrix operator*(T scalar) const
+    {
+        std::array<std::array<T, M>, N> array;
+        for (uint32_t m = 0; m < M; m++)
+        {
+            for (uint32_t n = 0; n < N; n++)
+            {
+                array[n][m] = m_matrix[n][m] * scalar;
+            }
+        }
+        return {array};
+    }
+
+private:
+    std::array<std::array<T, M>, N> m_matrix;
+};
+
+#endif //MATRICES_LIBRARY_H
---
+++ tests/addition.cpp
@@ -0,0 +1,85 @@
+#include <gtest/gtest.h>
+
+#include <matrix.h>
+
+TEST(MatrixTests, Addition)
+{
+    typedef Matrix<uint32_t, 1, 2> Matrix1x2;
+    Matrix1x2 m1{{11, 0}};
+    Matrix1x2 m2{{2, 3}};
+    Matrix1x2 m3{m1 + m2};
+    Matrix1x2 m4{{13, 3}};
+    EXPECT_EQ(m4, m3);
+}
+
+TEST(MatrixTests, Subtraction)
+{
+    typedef Matrix<int32_t, 1, 2> MatrixFloat1x2;
+    MatrixFloat1x2 m1{{11, 0}};
+    MatrixFloat1x2 m2{{2, 3}};
+    MatrixFloat1x2 m3{m1 - m2};
+    MatrixFloat1x2 m4{{9, -3}};
+    EXPECT_EQ(m4, m3);
+}
+
+TEST(MatrixTests, Transpose)
+{
+    typedef Matrix<int32_t, 1, 2> Matrix1x2;
+    typedef Matrix<int32_t, 2, 1> Matrix2x1;
+    Matrix1x2 m1{{11, 1}};
+    Matrix2x1 m2{m1.transpose()};
+    Matrix1x2 m3{m2.transpose()};
+    EXPECT_EQ(m1, m3);
+
+    typedef Matrix<uint32_t, 3, 5> Matrix3x5;
+    typedef Matrix<uint32_t, 5, 3> Matrix5x3;
+    Matrix3x5 m4{
+        {
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12},
+                {13, 14, 15},
+            }
+        }
+    };
+    Matrix5x3 m5{m4.transpose()};
+    Matrix3x5 m6{m5.transpose()};
+    EXPECT_EQ(m4, m6);
+}
+
+TEST(MatrixTests, Scalar)
+{
+    typedef Matrix<uint32_t, 3, 5> Matrix3x5;
+    Matrix3x5 m1{
+            {
+                {
+                    {1, 2, 3},
+                    {4, 5, 6},
+                    {7, 8, 9},
+                    {10, 11, 12},
+                    {13, 14, 15},
+                }
+            }
+    };
+    Matrix3x5 m2{
+                {
+                    {
+                        {2, 4, 6},
+                        {8, 10, 12},
+                        {14, 16, 18},
+                        {20, 22, 24},
+                        {26, 28, 30},
+                    }
+                }
+    };
+    EXPECT_EQ(m1 * 2, m2);
+    // EXPECT_EQ(2 * m1, m2);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
